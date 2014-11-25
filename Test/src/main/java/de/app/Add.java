120b650e2dbd8f8bf452b1beec7be0cda0ca79d0
Mirko Anton <elec-62@mail.ru>
2014-11-25 18:23:00 +0200

    Jin3

--- Test/src/main/java/de/app/Add.java
+++ Test/src/main/java/de/app/Add.java
@@ -1,3 +1,4 @@
+package de.app;
 
 
 public class Add {
---
+++ Test/src/test/java/TestAdd.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import de.app.Add;
+
+public class TestAdd {
+	Add adder = new Add();
+
+	@Test
+	public void test() {
+		 
+		assertEquals( 5, adder.add(2,3));
+	}
+	@Test
+	public void testok()
+	{
+		assertEquals(6, adder.add(3, 3));
+	}
+	@Test
+	public void testFailed()
+	{
+		assertNotEquals(6, adder.add(5, 5));
+	}
+	
+
+}
